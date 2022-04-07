311f3a11dc79c1d747c9a93f47a2312bfc046e6b
Fernando Futila <fernando.futila@gmail.com>
2022-04-07 19:10:23 +0100

    updated eslint and prettier configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,35 +3,35 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
-  parser: "@babel/eslint-parser",
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
     requireConfigFile: false,
     ecmaFeatures: {
       jsx: true,
     },
     babelOptions: {
-      presets: ["@babel/preset-react"],
+      presets: ['@babel/preset-react'],
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "react-hooks", "prettier"],
+  plugins: ['react', 'react-hooks', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".js"] }],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "import/prefer-default-export": "off",
-    "no-param-reassign": "off",
-    "jsx-a11y/label-has-associated-control": "off",
-    "no-unused-vars": "off",
-    "no-alert": "off",
-    "react/jsx-no-bind": [
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'no-unused-vars': 'off',
+    'no-alert': 'off',
+    'react/jsx-no-bind': [
       0,
       {
         allowFunctions: true,
       },
     ],
   },
-};
+}
